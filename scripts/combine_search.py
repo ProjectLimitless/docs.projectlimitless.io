ef7700438a5cd59b989ff7a7cf4de4607c448e08
Donovan Solms <donovan.solms@gmail.com>
2016-11-17 15:36:24 +0200

    Combine search indexes. Fixes #2

--- scripts/combine_search.py
+++ scripts/combine_search.py
@@ -2,13 +2,14 @@ import os
 import os.path
 import json
 
-print('Combining multiple search indexes')
+print('Combining multiple search indexes...')
 
 items = []
 
 for dirpath, dirnames, filenames in os.walk("./out"):
     for filename in [f for f in filenames if f.endswith("search_index.json")]:
         json_file = os.path.join(dirpath, filename)
+        # Skip the combined file if it exists
         if json_file == './out/mkdocs/search_index.json':
             continue
         with open(json_file) as json_data:
